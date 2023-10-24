18829c3ed6e902aa6f9f5d1ec7887e0a3fa2e3f4
BrunoCiprianoDev <cipriano990@gmail.com>
2023-10-23 22:54:07 -0300

    feat(SimplexSolver): Add all operations

--- server/src/runner.ts
+++ server/src/runner.ts
@@ -4,10 +4,10 @@ import { SimplexSolver } from "./service/SimplexSolver";
 const solver = new SimplexSolver();
 
 // Defina a função objetiva e restrições
-const objectiveFunction = [-3, -5, 0, 0, 0, 0]; // Exemplo de função objetiva: 2x1 + 3x2 + x3
-const restriction1 = [1, 0, 1, 0, 0, 4]; // Exemplo de restrição 1: x1 + x3 <= 1
-const restriction2 = [0, 2, 0, 1, 0, 12]; // Exemplo de restrição 2: 2x2 + 2x3 <= 3
-const restriction3 = [2, 3, 0, 0, 1, 21];
+const objectiveFunction = [-100, -80, -120, -30, 0, 0, 0, 0]; // Exemplo de função objetiva: 2x1 + 3x2 + x3
+const restriction1 = [1, 1, 1, 4, 1, 0, 0, 300]; // Exemplo de restrição 1: x1 + x3 <= 1
+const restriction2 = [0, 1, 1, 2, 0, 1, 0, 600]; // Exemplo de restrição 2: 2x2 + 2x3 <= 3
+const restriction3 = [3, 2, 4, 0, 0, 0, 1, 500];
 
 // Adicione a função objetiva e as restrições à instância do SimplexSolver
 solver.createObjetiveFunction(objectiveFunction);
@@ -15,14 +15,8 @@ solver.insertRestrictions(restriction1);
 solver.insertRestrictions(restriction2);
 solver.insertRestrictions(restriction3);
 
-
-// const indexEntryColumn = solver.getIndexEntryColumn();
-// const indexPivotLine = solver.getPivotLine(indexEntryColumn);
-// let pivotLine = solver.getLineByIndex(indexPivotLine);
-// pivotLine = solver.calculateNewPivotLine(indexEntryColumn, pivotLine);
-
-// const updatedLine = solver.updateLine([-3, -5, 0, 0, 0, 0], indexEntryColumn, pivotLine);
-//console.log(updatedLine);
-//line: number[], entryColumn: number, pivotLine: number[]
-
+solver.createListVariables(4);
 solver.calculate();
+
+solver.printTable();
+solver.getResults();
